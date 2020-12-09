59b60f62dfc119cae51d05536cb0b29e41dbaefc
rdehaggart1 <rdeh10@googlemail.com>
2020-12-09 15:08:31 +0000

    changed comment style
    i changed the actual descriptive comments to /* from //.
    couple of reasons: better contrast against the code for easier readability; better for copy/pasting without having to recomment sections; better for easy splits if we want to; etc.

--- PDC/PDC.ino
+++ PDC/PDC.ino
@@ -9,9 +9,9 @@
    The PDC is an Arduino Nano serving the dual purpose of parachute deployment activities (e.g. apogee detection), and attitude determination.
 */
 
-// the accel/gyro, barometer & micro-SD unit are on SPI, so include library for SPI commands (https://www.arduino.cc/en/reference/SPI)
+/* the accel/gyro, barometer & micro-SD unit are on SPI, so include library for SPI commands (https://www.arduino.cc/en/reference/SPI) */
 #include <SPI.h>
-// we want the SD card library too (https://www.arduino.cc/en/reference/SD)
+/* we want the SD card library too (https://www.arduino.cc/en/reference/SD) */
 #include <SD.h>
 
 /* ---------- SPI CONFIG ---------- */
@@ -25,12 +25,12 @@
  * 3. all devices are compatible with mode 00 (clock idle low, output: falling edge, capture: rising edge);
  */
  
-// this is then our object with settings for our transactions
+/* this is then our object with settings for our transactions */
 SPISettings SPIParams(10000000, MSBFIRST, SPI_MODE0); 
 
-// the arduino nano has an 'SS' pin (10) which helps us choose if we want to be master or slave. pin 10 as output = PDC as master
+/* the arduino nano has an 'SS' pin (10) which helps us choose if we want to be master or slave. pin 10 as output = PDC as master */
 const int PDC_SS = 10;
-// define the DIGIN pins on the PDC that are connected to the 'slave select' (SS) pin of each device.
+/* define the DIGIN pins on the PDC that are connected to the 'slave select' (SS) pin of each device */
 const int altimeter_SS = 4;
 const int IMU_SS = 5;
 const int microSD_SS = 6;
@@ -41,29 +41,29 @@ const int microSD_SS = 6;
 /* various device configurations to setup communications and verify that things are working and ready to go */
 void setup() {
 
-  // to store the return value of the accelerometer 'WHO_AM_I' identification register
+  /* to store the return value of the accelerometer 'WHO_AM_I' identification register */
   unsigned int IMU_WHO_AM_I;
-  // to store the return value of the altimeter 'CHIP_ID' identification register
+  /* to store the return value of the altimeter 'CHIP_ID' identification register */
   unsigned int altimeter_CHIP_ID;
-  // new instance of the 'File' class (part of the SD library) that we will use to control the .csv file on the microSD card
+  /* new instance of the 'File' class (part of the SD library) that we will use to control the .csv file on the microSD card */
   File dataLogFile;
   
   /* ---------- Serial Setup ---------- */
-  // open serial comms at 9600 baud to allow us to monitor the process
-    // serial may become irrelevant - once the code is on the PDC we might not be connecting via serial
-    // but it's useful for ground testing
+  /* open serial comms at 9600 baud to allow us to monitor the process
+       serial may become irrelevant - once the code is on the PDC we might not be connecting via serial
+       but it's useful for ground testing */
   Serial.begin(9600);
   while(!Serial){
-    ; // wait for port to connect
+    ; /* wait for port to connect */
   }
 
   /* ---------- SPI Setup ---------- */
-  // we want to be the master of this bus! so set the 'SS' pin on the PDC as an output
+  /* we want to be the master of this bus! so set the 'SS' pin on the PDC as an output */
   pinMode(PDC_SS, OUTPUT);
   
-  // set each slave select pin as an output. 
-    // initialise each pin to be high (i.e. communication disabled)
-    // to communicate with a specific device, take its SS pin low with digitalWrite(device_SS, LOW);
+  /* set each slave select pin as an output. 
+       initialise each pin to be high (i.e. communication disabled)
+       to communicate with a specific device, take its SS pin low with digitalWrite(device_SS, LOW); */
   pinMode(altimeter_SS, OUTPUT);
   digitalWrite(altimeter_SS, HIGH);
   pinMode(IMU_SS, OUTPUT);
@@ -71,7 +71,7 @@ void setup() {
   pinMode(microSD_SS, OUTPUT);
   digitalWrite(microSD_SS, HIGH);
   
-  // Initialise all lines and CPU to use SPI
+  /* initialise all lines and CPU to use SPI */
   SPI.begin();
 
   /* ---------- I2C Setup ---------- */
@@ -79,11 +79,10 @@ void setup() {
 
   /* ---------- SPI Verification ---------- */
   
-  // communicate with altimeter: read the 'CHIP_ID' register. expect 0x50
+  /* communicate with altimeter: read the 'CHIP_ID' register. expect 0x50 */
   altimeter_CHIP_ID = readSPI(altimeter_SS, 0x00, 1);
-  // we have read the 'CHIP_ID' register and now should check that the value read is as we expect
-    // TODO: when a little more developed, this should be replaced with something more meaningful! e.g. comms to ground
-    // to indicate 'yes we're good on the altimeter'
+  /* we have read the 'CHIP_ID' register and now should check that the value read is as we expect */
+  // TODO: when a little more developed, this should be replaced with something more meaningful! e.g. comms to ground to indicate 'yes we're good on the altimeter'
   if (altimeter_CHIP_ID == 0x50) {
     Serial.println("Altimeter successfully connected!");
   }
@@ -91,10 +90,10 @@ void setup() {
     Serial.println("Altimeter could not be reached!");
   }
   
-  // communicate with IMU: read the 'WHO_AM_I' register. expect 0110110
+  /* communicate with IMU: read the 'WHO_AM_I' register. expect 0110110 */
   IMU_WHO_AM_I = readSPI(IMU_SS, 0x0f, 1);
-  // we have read the 'WHO_AM_I' register and now should check that the value read is as we expect
-    // TODO: when a little more developed, this should be replaced with something more meaningful!
+  /* we have read the 'WHO_AM_I' register and now should check that the value read is as we expect */
+  // TODO: when a little more developed, this should be replaced with something more meaningful!
   if (IMU_WHO_AM_I == 0110110) {
     Serial.println("IMU successfully connected!");
   }
@@ -102,8 +101,8 @@ void setup() {
     Serial.println("IMU could not be reached!");
   }
   
-  // attempt to init micro SD card
-    // TODO: if we have a shield with 'CD' (chip detect) pin, make use of this to check pin is in place.
+  /* attempt to init micro SD card */
+  // TODO: if we have a shield with 'CD' (chip detect) pin, make use of this to check pin is in place.
   if(SD.begin(microSD_SS)) {
     Serial.println("micro-SD card initialised");
   }
@@ -111,18 +110,18 @@ void setup() {
     Serial.println("micro-SD initialisation failed!");
   }
   
-  // attempt to open a .csv file which we want to log data to
-    // TODO: once RTC is up & running, name the file with timestamp as per ISO 8601 format (kind of..)(yyyy-mm-ddThh-mm-ss.csv)
+  /* attempt to open a .csv file which we want to log data to */
+  // TODO: once RTC is up & running, name the file with timestamp as per ISO 8601 format (kind of..)(yyyy-mm-ddThh-mm-ss.csv)
   dataLogFile = SD.open("temp.csv", FILE_WRITE);
 
-  // if the file can't open, the return is false
+  /* if the file can't open, the return is false */
   if(!dataLogFile) {
     Serial.println("Data log file could not be opened!");
   }
 
-  // communicate with micro SD - write the csv headers to our file
-    // should define (either here or elsewhere) the units of each of these headers... perhaps in readme
-    // TODO: populate this more fully - what are the raw measurements from BMP, GYRO, light sensors, etc?
+  /* communicate with micro SD - write the csv headers to our file
+       should define (either here or elsewhere) the units of each of these headers... perhaps in readme */
+  // TODO: populate this more fully - what are the raw measurements from BMP, GYRO, light sensors, etc?
   dataLogFile.print("Date, \
                      Time, \
                      acc_x, \
@@ -164,54 +163,54 @@ void loop() {
 
 /* read a value from a register of a device on SPI. as arguments, pass the device select pin, the address of the register, and the number of bytes that this
    register contains. it will return the value that is stored in the register that we are reading */
+
 unsigned int readSPI(int deviceSelect, byte registerSelect, int numBytes) {
 
-  // variable for our register value return
+  /* variable for our register value return */
   unsigned int result = 0; 
 
-  // the 'read' or 'write' bit is in different positions for different devices but then the register address then occupies the rest of the field, 
-    // so we can shift the register up into place when the R/W occupies LSB 
-    // read = 1, write = 0
+  /* the 'read' or 'write' bit is in different positions for different devices but then the register address occupies the rest of the field, 
+       so we can shift the register up into place when the R/W occupies LSB 
+       read = 1, write = 0 */
   if (deviceSelect == IMU_SS) {
-    // r/w in LSB spot
+    /* r/w in LSB spot */
     registerSelect = (registerSelect << 1) | 1;
   }
   else if (deviceSelect == altimeter_SS) {
-    // r/w in MSB spot
+    /* r/w in MSB spot */
     registerSelect = registerSelect | (1 << 8); 
   }
   else {
     Serial.println("ERROR: device does not exist on SPI");
   }
   
-  // begin a transaction over SPI using our params. this command also stops interrupts from preventing SPI comms
+  /* begin a transaction over SPI using our params. this command also stops interrupts from preventing SPI comms */
   SPI.beginTransaction(SPIParams);
   
-  // to communicate with the IMU, we take its slave select pin on the PDC low
+  /* to communicate with the IMU, we take its slave select pin on the PDC low */
   digitalWrite(deviceSelect, LOW);
 
-  // if we want to read a particular address, we must send the address of the register to the device
+  /* if we want to read a particular address, we must send the address of the register to the device */
   SPI.transfer(registerSelect);
 
-  // now if we send nothing, we are listening for the result - the device will send the value in the register we requested
-  // for the first byte, just read the value into 'result'
+  /* now if we send nothing, we are listening for the result - the device will send the value in the register we requested for the first byte, just read the value into 'result' */
   result = SPI.transfer(0x00);
-  // decrement the number of bytes that we have left to read
+  /* decrement the number of bytes that we have left to read */
   numBytes--;
   
   while(numBytes != 0) {
-    // if we have more than one byte to read, shift the result so far up by a byte, and fill the empty space with our new byte
+    /* if we have more than one byte to read, shift the result so far up by a byte, and fill the empty space with our new byte */
     result = (result << 8) | SPI.transfer(0x00);
-    // decrement the number of bytes until we get to zero, when this while() will exit
+    /* decrement the number of bytes until we get to zero, when this while() will exit */
     numBytes--;
   }
 
-  // stop communications with IMU by setting the corresponding slave select on the PDC to high
+  /* stop communications with IMU by setting the corresponding slave select on the PDC to high */
   digitalWrite(deviceSelect, HIGH);
   
-  // we're done now! restart interrupt mechanisms
+  /* we're done now! restart interrupt mechanisms */
   SPI.endTransaction();
 
-  // send our address value back to the caller
+  /* send our address value back to the caller */
   return(result);
 }
