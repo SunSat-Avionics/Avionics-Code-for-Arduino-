3a5c082b1e0c25c102e61a0ca5ccc2c64d326d95
rdehaggart1 <rdeh10@googlemail.com>
2020-12-16 21:37:33 +0000

    IMU class updates

--- PDC/PDC_mainCode/IMU_classTesting/PDC_IMU.cpp
+++ PDC/PDC_mainCode/IMU_classTesting/PDC_IMU.cpp
@@ -7,8 +7,8 @@
 
 #include "PDC_SPI.h"
 
-PDC_IMU::PDC_IMU(const int CS){
-	self.slaveSelect = CS;
+void PDC_IMU::PDC_IMU(const int CS){
+	slaveSelect = CS;
 }
 
 bool PDC_IMU::isAlive(){
@@ -20,30 +20,34 @@ bool PDC_IMU::isAlive(){
 
 /* read the z-axis acceleration and convert to an acceleration in m/s2 */
 float PDC_IMU::readAccelerationZ(){
-  /* the resolution of the accelerometer is its range (e.g. +/4g) divided by the number of combinations available
-     the accelerometer output is 16bits, so 2^16 is our combinations. 
-      units are milli-g per bit, so times 1000*/
-  float accelResolution = (self.measurementRange*2 / 65536) * 1000;
+	/* the resolution of the accelerometer is its range (e.g. +/4g) divided by the number of combinations available
+	 the accelerometer output is 16bits, so 2^16 is our combinations. 
+	  units are milli-g per bit, so times 1000*/
+	float accelResolution = (measurementRange * 2 / 65536) * 1000;
 
-  /* for the output from the accelerometer */
-  int rawAccelZ;
-  /* for the actual acceleration in g */
-  float acceleration = 0;
+	/* for the output from the accelerometer */
+	int rawAccelZ;
+	/* for the actual acceleration in g */
+	float acceleration = 0;
 
-  /* read z-axis acceleration.
-       note in CTRL3_C, there is a default enabled bit which auto-increments the register address when reading multiple bytes so we dont need to read the H and L
-       registers separately ! */
-  rawAccelZ = readSPI(IMU_SS, 0x2C, 2);
+	/* read z-axis acceleration.
+	   note in CTRL3_C, there is a default enabled bit which auto-increments the register address when reading multiple bytes so we dont need to read the H and L
+	   registers separately ! */
+	rawAccelZ = readSPI(IMU_SS, 0x2C, 2);
 
-  /* convert our output into an actual acceleration value in ms/2 */
-  acceleration = (rawAccelZ * 9.80665 * accelResolution) / 1000;
+	/* convert our output into an actual acceleration value in ms/2 */
+	acceleration = (rawAccelZ * 9.80665 * accelResolution) / 1000;
 
-  return (acceleration);
+	return (acceleration);
 }
 
 void PDC_IMU::setMeasurementRange(int range){
-	self.measurementRange = range;
+	measurementRange = range;
 	// TODO: actually write to controller (requires an SPI write function!)
+}
+
+int PDC_IMU::getMeasurementRange(){
+	return(measurementRange);
 }
 
 void PDC_IMU::measureAccelerometerNoiseZ(int numReadings){
--- PDC/PDC_mainCode/IMU_classTesting/PDC_IMU.h
+++ PDC/PDC_mainCode/IMU_classTesting/PDC_IMU.h
@@ -1,22 +1,25 @@
 float readAccelerationZ();
 
 class PDC_IMU{
-  public:
-	const int slaveSelect;
-    int measurementRange;
+	private:
+		const int slaveSelect;
+		int measurementRange;
 	
-	/* constructor */
-    PDC_IMU(const int CS){
-		slaveSelect = CS;
-	}; 
-	/* check if connected and responsive */
-	bool isAlive();
-	/* read the acceleration in z-direction (m/s2) */
-    float readAccelerationZ();
-	/* set the measurement range (+/- range g) */
-	void setMeasurementRange(int range);
-	/* measure the RMS noise in the z-direction of the accelerometer for a given number of readings */
-	float measureAccelerometerNoiseZ(int numReadings);
+	public:
+		/* constructor */
+		void PDC_IMU(const int CS){
+			slaveSelect = CS;
+		}; 
+		/* check if connected and responsive */
+		bool isAlive();
+		/* read the acceleration in z-direction (m/s2) */
+		float readAccelerationZ();
+		/* set the measurement range (+/- range g) */
+		void setMeasurementRange(int range);
+		/* get the measurement range (doesn't directly access the device, just reads class attribute */
+		int getMeasurementRange();
+		/* measure the RMS noise in the z-direction of the accelerometer for a given number of readings */
+		float measureAccelerometerNoiseZ(int numReadings);
 }
 
 /* Example usage 
@@ -24,7 +27,8 @@ class PDC_IMU{
 PDC_IMU IMU(10); where the argument is the CS pin
 flag = IMU.isAlive(); if flag is 0, we're all good
 IMU.setMeasurementRange(4); (+/- in g)
+measurementRange = IMU.getMeasurementRange(); get the range of measurements
 IMU.measureAccelerometerNoiseZ(10); measure the noise in the z direction for 10 samples
-
+accZ = IMU.readAccelerationZ(); get the acceleration in m/s2 in the z-direction
 
 */
